bed368dc3128c450569c58754d395f34efad9746
masayufrnstkaa <masayufrnstkaa@gmail.com>
2024-10-01 08:07:51 +0700

    update database migation

--- routes/tables.php
+++ routes/tables.php
@@ -10,9 +10,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/tables/basic', [Basic::class, 'index'])->name('tables-basic');
 Route::get('/tables/chopper', [ChopperTable::class, 'index'])->name('tables-chopper');
 Route::get('/tables/bajak', [BajakController::class, 'index'])->name('tables-bajak');
-<<<<<<< HEAD
 Route::get('/tables/subsoil', [SubsoilController::class, 'index'])->name('tables-subsoil');
-=======
-
-// hmm
->>>>>>> 897cac00502908ae7bee8210741cda4f9ee9e9b4
